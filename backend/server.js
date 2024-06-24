e1e61ac5bcfae6f5e8084b08937a10845881ad25
CSBS26 <148430455+CSBS26@users.noreply.github.com>
2024-06-24 21:04:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.PORT}/${process.env.DB_DATABASE}`;
+const urlDB = 'mysql://root:WJhPdhLolEObeVUuhewLNmpSrEfNeHNU@viaduct.proxy.rlwy.net:13789/railway';
 
 const connection = mysql.createConnection(urlDB);
 
